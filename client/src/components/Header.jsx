49ecb2e78d6427bf5ff2180677b820c51a0a3547
robindecroock <decroock.robin@gmail.com>
2023-04-08 15:06:14 +0200

    added background video instead of photo if found

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import sample from '../assets/video/chiro.mp4'
 const Header = () => {
   return (
     <header>
         <h1><span class="light">Chiro Popov Bouwt aan</span><br/>nieuwe lokalen</h1>
+        <video className='videoTag' autoPlay loop muted>
+    <source src={sample} type='video/mp4' />
+</video>
     </header>
   )
 }
